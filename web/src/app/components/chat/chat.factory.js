aa475f0a1dd7cbe17c85081d34b4460e1284e74e
Nebojsa Stojanovic <nebojsa.stojanovic@gauss.hr>
2015-12-08 11:01:50 +0100

    bugfix

--- web/src/app/components/chat/chat.factory.js
+++ web/src/app/components/chat/chat.factory.js
@@ -6,11 +6,17 @@
         .factory('Chat', Chat);
 
 
-    function Chat (LocalService) {
-        var socket = io.connect('http://localhost:1337', {query: "__sails_io_sdk_version=0.11.0"});
+    function Chat(LocalService) {
+        var socket = io.connect('http://52.16.220.86:667', {query: "__sails_io_sdk_version=0.11.0"});
         var token = angular.fromJson(LocalService.get('auth_token')).token;
 
         return {
+            connect: function (cb) {
+                return socket.on('connect', function () {
+                    socket.emit("get", {url: "/api/message/connect", data: {token: token}}, cb);
+                });
+            },
+
             sendMsg: function (message, cb) {
                 return socket.emit('post', {
                     url: '/api/message/create',
