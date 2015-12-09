d7433debbf1eb47e6e9585d6d034fab84114c85f
hlozancic <hlozancic@gmail.com>
2015-12-09 15:35:44 +0100

    chat refractor

--- web/src/app/components/chat/chat.controller.js
+++ web/src/app/components/chat/chat.controller.js
@@ -6,44 +6,82 @@
         .controller('ChatController', ChatController);
 
     /** @ngInject */
-    function ChatController($scope, Chat, SweetAlert) {
+    function ChatController($scope, SweetAlert, LocalService, $state, API, Chat) {
+
         var vm = this;
+        var token = angular.fromJson(LocalService.get('auth_token')).token;
 
-        vm.messages = Chat.messageBuffer || [];
-        vm.chatConnected = Chat.connected || false;
+        vm.messages = [];
+
+        if (Chat.socket) {
+            getData();
+        } else {
+            Chat.socket = io.connect(API.URL, {query: "__sails_io_sdk_version=0.11.0"});
+        }
+
+        Chat.socket.on('connect', function () {
+            console.log("KONEKTO!");
+            getData();
+        });
 
 
-        Chat.connect(function (err, data) {
-            if(!err && !data) return;
-            if (err) return SweetAlert.swal('Chat Error', err.summary);
-
-            vm.chatConnected = true;
-
-            vm.messages = data;
+        // eventi
+        Chat.socket.on('disconnect', function () {
+            console.log("UMIREM");
+            vm.chatConnected = false;
             $scope.$apply();
+        });
 
-            Chat.onMsg(function (err, data) {
-                if (err) return SweetAlert.swal('Chat error', err.summary);
+        Chat.socket.on('reconnect_error', function () {
+            console.log("reconect error");
+            vm.chatConnected = false;
+            $scope.$apply();
+        });
+
+        Chat.socket.on('reconnect', function () {
+            console.log("reconect");
+            vm.chatConnected = true;
+            $scope.$apply();
+        });
+
+        Chat.socket.on("message", function (data) {
+            console.log("ide poruka");
+            if (data.statusCode && data.statusCode != 200) return SweetAlert.swal('Chat error', data.body.summary);
+
+            vm.messages.push(data.data);
+            $scope.$apply();
+        });
+
+
+        //emiti
+        vm.send = function (message) {
+            Chat.socket.emit('post', {
+                url: '/api/message/create',
+                data: {token: token, text: message.text}
+            }, function (data) {
+                if (data.statusCode != 200) return SweetAlert.swal('Chat error', data.body.summary);
                 $scope.$apply();
             });
+        };
 
-            Chat.reconnect_error(function() {
-               vm.chatConnected = false;
-                $scope.$apply();
-            });
 
-            Chat.reconnect(function() {
+        function getData() {
+            Chat.socket.emit("get", {url: "/api/message/connect", data: {token: token}}, function (data) {
+
+                if (data.statusCode === 401 || data.statusCode === 403) {
+                    Chat.socket.disconnect();
+                    LocalService.unset('auth_token');
+                    return $state.go('anon.login');
+                }
+
+                if (data.statusCode != 200) return alert("greska");
+
+                vm.messages = data.body;
                 vm.chatConnected = true;
                 $scope.$apply();
             });
-        });
+        }
 
-        vm.send = function (message) {
-            Chat.sendMsg(message, function (data) {
-                $scope.$apply();
-                if (data.statusCode != 200) return SweetAlert.swal('Chat error', data.body.summary);
-            });
-        };
     }
 
 })();
--- web/src/app/components/chat/chat.factory.js
+++ web/src/app/components/chat/chat.factory.js
@@ -7,72 +7,10 @@
 
 
     function Chat(LocalService, API, $state) {
-
-        var chat = {
-            messageBuffer: [],
-            socket: false,
-            token: false,
-            connected: false,
-            connect: function (cb) {
-
-                    chat.socket = io.connect(API.URL, {query: "__sails_io_sdk_version=0.11.0"});
-                    chat.token = angular.fromJson(LocalService.get('auth_token')).token;
-                    chat.connected = true;
-
-                console.log("KLONEKTAM");
-                    chat.socket.on('connect', function () {
-                        chat.socket.emit("get", {url: "/api/message/connect", data: {token: chat.token}}, function (data) {
-
-                            if (data.statusCode === 401 || data.statusCode === 403) {
-                                chat.socket.disconnect();
-                                LocalService.unset('auth_token');
-                                $state.go('anon.login');
-                            }
-
-                            if (data.statusCode != 200) return cb(data.body);
-                            chat.messageBuffer = data.body;
-
-                            return cb(null, data.body);
-                        });
-                    });
-
-
-                    chat.socket.on('disconnect', function () {
-                        console.log("UMIREM")
-                        chat.connected = false;
-                    });
-
-
-            },
-
-            reconnect_error: function(cb) {
-                chat.socket.on('reconnect_error', cb);
-                chat.connected = false;
-            },
-
-            reconnect: function(cb){
-                chat.socket.on('reconnect', cb);
-                chat.connected = true;
-            },
-
-            onMsg: function(cb) {
-                return chat.socket.on("message", function(data) {
-                     if(data.statusCode && data.statusCode != 200) return cb(data.body);
-
-                    chat.messageBuffer.push(data.data);
-                    cb(null, data.data);
-                });
-            },
-
-            sendMsg: function (message, cb) {
-                return chat.socket.emit('post', {
-                    url: '/api/message/create',
-                    data: {token: chat.token, text: message.text}
-                }, cb);
-            }
+        return {
+            socket: false
         };
 
-        return chat;
     }
 
 })();
