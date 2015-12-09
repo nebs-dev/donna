a00469d26020a68d98423d4e67be87e7377f2877
Nebojsa Stojanovic <nebojsa.stojanovic@gauss.hr>
2015-12-09 11:18:49 +0100

    dashboard events bugfix

--- web/src/app/components/dashboard/dashboard.contoller.js
+++ web/src/app/components/dashboard/dashboard.contoller.js
@@ -10,6 +10,7 @@
         var vm = this;
 
         Dashboard.getData().success(function (data) {
+            console.log(data);
             vm.data = data;
         }).error(function (data) {
             SweetAlert.swal(data.error, data.summary);
