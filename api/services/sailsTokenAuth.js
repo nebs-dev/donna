1de9c5cbb4d3e6e0fcecdc6e08333318d65ec653
Nebojsa Stojanovic <nebojsa.stojanovic@gauss.hr>
2015-12-09 11:23:51 +0100

    dashboard events bugfix

--- api/services/sailsTokenAuth.js
+++ api/services/sailsTokenAuth.js
@@ -4,11 +4,11 @@ module.exports = {
 
     getIp: function (req) {
         return req.headers['x-forwarded-for'] ||
-            req.connection.remoteAddress ||
-            req.socket.remoteAddress;
+            req.socket.remoteAddress
     },
-    //req.connection.socket.remoteAddress
+
     //    req.connection.remoteAddress ||
+    //req.connection.socket.remoteAddress;
 
     issueToken: function (payload) {
 
