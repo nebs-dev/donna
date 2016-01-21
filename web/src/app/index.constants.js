e028bd6e0d699dac8d7af6124c7e6c982470b0fc
Nebojsa Stojanovic <nebojsa.stojanovic@gauss.hr>
2016-01-21 12:34:39 +0100

    push notification android

--- web/src/app/index.constants.js
+++ web/src/app/index.constants.js
@@ -5,7 +5,7 @@
         .module('donna')
         .constant('API', {
             //URL: 'http://localhost:1337',
-            URL: 'http://52.16.220.86:667',
+            URL: 'http://52.31.77.24:667',
             //URL: 'http://donnavekicapp.com:666',
 
             formData: function (data) {
